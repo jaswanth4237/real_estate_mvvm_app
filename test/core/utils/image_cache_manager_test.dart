e5ead9cf83eddc3a530f1db592f61428a5b258ce
jaswanth4237 <24p35a4237@acet.ac.in>
2026-03-14 10:19:37 +0530

    test: fix path_provider mock and import in test/core/utils/image_cache_manager_test.dart

--- test/core/utils/image_cache_manager_test.dart
+++ test/core/utils/image_cache_manager_test.dart
@@ -1,8 +1,20 @@
 import 'dart:io';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:real_estate_mvvm_app/core/utils/app_data_path.dart';
 import 'package:real_estate_mvvm_app/core/utils/image_cache_manager.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(
+      const MethodChannel('plugins.flutter.io/path_provider'),
+      (MethodCall methodCall) async => Directory.systemTemp.path,
+    );
+  });
+
   test('ImageCacheManager should create stats file', () async {
     await ImageCacheManager.logCacheStats(
       totalImages: 10,
@@ -10,7 +22,7 @@ void main() {
       hitRate: 0.9,
     );
 
-    final file = File('app_data/image_cache_stats.json');
+    final file = await AppDataPath.getFile('image_cache_stats.json');
     expect(await file.exists(), true);
   });
 }
