b6158f684aeb48528a46f09aa855f9c5b1bbade3
jaswanth4237 <24p35a4237@acet.ac.in>
2026-03-14 10:19:37 +0530

    test: fix path_provider mock and import in test/core/accessibility/accessibility_service_test.dart

--- test/core/accessibility/accessibility_service_test.dart
+++ test/core/accessibility/accessibility_service_test.dart
@@ -1,16 +1,25 @@
 import 'dart:io';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:real_estate_mvvm_app/core/accessibility/accessibility_service.dart';
+import 'package:real_estate_mvvm_app/core/di/accessibility/accessibility_service.dart';
+import 'package:real_estate_mvvm_app/core/utils/app_data_path.dart';
 
 class MockSharedPreferences extends Mock implements SharedPreferences {}
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
   late AccessibilityService service;
   late MockSharedPreferences mockPrefs;
 
   setUp(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(
+      const MethodChannel('plugins.flutter.io/path_provider'),
+      (MethodCall methodCall) async => Directory.systemTemp.path,
+    );
     mockPrefs = MockSharedPreferences();
     service = AccessibilityService(mockPrefs);
   });
@@ -27,7 +36,7 @@ void main() {
     ]);
 
     // assert
-    final file = File('app_data/accessibility_report.json');
+    final file = await AppDataPath.getFile('accessibility_report.json');
     expect(await file.exists(), true);
   });
 }
