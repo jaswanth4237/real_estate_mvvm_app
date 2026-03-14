d64dce8d40efb84790b143e0fd0a1f0d8d8bbb12
jaswanth4237 <24p35a4237@acet.ac.in>
2026-03-14 10:19:37 +0530

    test: fix path_provider mock and import in test/core/error/error_logger_test.dart

--- test/core/error/error_logger_test.dart
+++ test/core/error/error_logger_test.dart
@@ -1,15 +1,27 @@
 import 'dart:io';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:real_estate_mvvm_app/core/error/error_logger.dart';
+import 'package:real_estate_mvvm_app/core/utils/app_data_path.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(
+      const MethodChannel('plugins.flutter.io/path_provider'),
+      (MethodCall methodCall) async => Directory.systemTemp.path,
+    );
+  });
+
   test('ErrorLogger should create file and log error', () async {
     await ErrorLogger.logError(
       errorType: 'TestErr',
       message: 'msg',
     );
 
-    final file = File('app_data/error_logs.json');
+    final file = await AppDataPath.getFile('error_logs.json');
     expect(await file.exists(), true);
   });
 }
